0e13aaaea2c504eb0e2e0d55ceb37888f121e081
B.Krishna vetthi <30472833+krishnavetthi@users.noreply.github.com>
2021-04-15 17:57:06 +0530

    Arithmetic, Increment & Decrement Operators in JS

---
+++ operators.js
@@ -0,0 +1,44 @@
+//Arithmetic Operator Javascript
+//Addition, Subtraction, Multiplication, Division
+
+//Addition and Subtraction of two numbers
+
+var number1 = 10;
+var number2 = 5;
+
+console.log("Addition of the two numbers:");
+console.log(number1+number2);
+
+console.log("Subraction of the two numbers:");
+console.log(number1-number2);
+
+//Assigment - perform division and multiplication
+
+
+//Increment, Decrement Operators
+
+var number = 1;
+number = number + 1;
+console.log(number);
+
+//Increment Operator
+number++; // same as number = number + 1;
+console.log(number);
+
+//Decrement Opeartor
+number--;
+console.log(number);
+
+
+//Average of subjects of a student
+
+var maths = 100;
+var physics = 98;
+var chemistry = 90;
+
+var sumofall = maths + physics + chemistry;
+var average = sumofall/3;
+
+console.log("Average of 3 subjects = "+ average);
+
+//Assignement : Find the percentage of your marks writing  JS code
