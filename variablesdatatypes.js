e1c9b32e629343494d2b43c7b848a2ddfb7d0f2d
B.Krishna vetthi <30472833+krishnavetthi@users.noreply.github.com>
2021-04-15 15:34:37 +0530

    Variables and datatypes

---
+++ variablesdatatypes.js
@@ -0,0 +1,29 @@
+/*      Variables and datatyes
+datatypes - number, string, boolean, null, undefined 
+var, const and let - keywords   */
+
+//declare and initialise variable
+
+//Using var keyword
+ var number = 10;
+ var nothing = null;
+
+//Using let keyword
+let name = "Krishna";
+let notdefined;
+
+//Using const keyword
+const pi = 3.14;
+
+console.log(pi);
+
+name = 'vetthi';
+console.log(name);
+
+console.log(number);
+console.log(notdefined);
+console.log(nothing)
+
+
+
+
