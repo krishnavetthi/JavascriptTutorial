9c54cc7789c9f1670d4d3d319b011d0d5915f241
B.Krishna vetthi <30472833+krishnavetthi@users.noreply.github.com>
2021-04-15 19:19:26 +0530

    Logical AND and OR

---
+++ logicaloperators.js
@@ -0,0 +1,14 @@
+//Logical Operators
+var number1 = 70, number2 = 21;
+
+//Logical AND - if both the conditionals are true then True otherwise False
+if(number1%10 && number2%10){
+    console.log("The number is divisible by 10");
+}
+
+//Logical OR - if both the conditions false then False otherwise True
+if(number1%10 || number2%10){
+    console.log("The number is divisible by 10");
+}
+
+
